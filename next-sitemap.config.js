2ac74362a5650f89e90ea67b4178d75967287da3
Glorry Sibomana <glorrysibomana758@gmail.com>
2025-04-10 22:41:14 +0300

    sitemap config

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://glorry.com',
+    generateRobotsTxt: true,
+    // Add any other sitemap config options here
+  };
---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['your-supabase-storage-url.supabase.co'],
+    },
+    // Add any other Next.js config options here
+  };
+  
+  export default nextConfig;
